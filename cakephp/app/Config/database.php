f26dc2eba5c1153b57abe234702092c9e765bcbe
Ekkart Kleinod <ekleinod@edgesoft.de>
2012-07-29 22:25:55 +0200

    configuration files

---
+++ cakephp/app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'refereemanager',
+		'prefix' => 'rfrmgr_',
+	);
+}
