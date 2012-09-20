ec359e8fdcae847de4e30790d5686c2b3b134329
Ekkart Kleinod <ekleinod@edgesoft.de>
2012-09-21 00:35:14 +0200

    first change of assignments index, very slow, very annoying

--- cakephp/app/Controller/AssignmentsController.php
+++ cakephp/app/Controller/AssignmentsController.php
@@ -14,13 +14,26 @@ class AssignmentsController extends AppController {
  * @return void
  */
 	public function index($season = null) {
-		$this->Assignment->recursive = 0;
 
+		// select season
 		if ($season == null) {
 			$season = sprintf('%d', ((idate('m') < 8) ? (idate('Y') - 1) : idate('Y')));
 		}
+		$options['conditions'] = array(
+			'Season.year_start' => $season
+		);
 
-		$this->set('assignments', $this->paginate());
+		// find matching assignments
+		$assignments = $this->Assignment->find('all', $options);
+
+		// add missing fields
+		$this->loadModel('League');
+		foreach ($assignments as &$assignment):
+			$assignment['League'] = $this->League->findById($assignment['Assignment']['league_id']);
+		endforeach;
+
+		// pass selected items to view
+		$this->set('assignments', $assignments);
 		$this->set('season', $season);
 	}
 
@@ -108,97 +121,4 @@ class AssignmentsController extends AppController {
 		$this->redirect(array('action' => 'index'));
 	}
 
-/**
- * assignments_index method
- *
- * @return void
- */
-	public function assignments_index() {
-		$this->Assignment->recursive = 0;
-		$this->set('assignments', $this->paginate());
-	}
-
-/**
- * assignments_view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function assignments_view($id = null) {
-		$this->Assignment->id = $id;
-		if (!$this->Assignment->exists()) {
-			throw new NotFoundException(__('Invalid assignment'));
-		}
-		$this->set('assignment', $this->Assignment->read(null, $id));
-	}
-
-/**
- * assignments_add method
- *
- * @return void
- */
-	public function assignments_add() {
-		if ($this->request->is('post')) {
-			$this->Assignment->create();
-			if ($this->Assignment->save($this->request->data)) {
-				$this->Session->setFlash(__('The assignment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The assignment could not be saved. Please, try again.'));
-			}
-		}
-		$seasons = $this->Assignment->Season->find('list');
-		$this->set(compact('seasons'));
-	}
-
-/**
- * assignments_edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function assignments_edit($id = null) {
-		$this->Assignment->id = $id;
-		if (!$this->Assignment->exists()) {
-			throw new NotFoundException(__('Invalid assignment'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Assignment->save($this->request->data)) {
-				$this->Session->setFlash(__('The assignment has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The assignment could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Assignment->read(null, $id);
-		}
-		$seasons = $this->Assignment->Season->find('list');
-		$this->set(compact('seasons'));
-	}
-
-/**
- * assignments_delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function assignments_delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Assignment->id = $id;
-		if (!$this->Assignment->exists()) {
-			throw new NotFoundException(__('Invalid assignment'));
-		}
-		if ($this->Assignment->delete()) {
-			$this->Session->setFlash(__('Assignment deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Assignment was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
 }
---
+++ cakephp/app/View/Assignments/index.ctp
@@ -0,0 +1,46 @@
+<div class="assignments index">
+	<h2><?php echo __('Referee Assignments'); ?></h2>
+	<p>Season: <?php echo $season; ?><!--?php echo $this->Html->link($assignment['Season']['year_start'], array('controller' => 'seasons', 'action' => 'view', $assignment['Season']['id'])); ?--></p>
+	<table>
+		<thead>
+			<tr>
+				<th><?php echo __('Date'); ?></th>
+				<th><?php echo __('Time'); ?></th>
+				<th><?php echo __('Game-No.'); ?></th>
+				<th><?php echo __('Ligue'); ?></th>
+				<th><?php echo __('Home Team'); ?></th>
+				<th><?php echo __('Off Team'); ?></th>
+				<th><?php echo __('OSR'); ?></th>
+				<th><?php echo __('Stellv. OSR'); ?></th>
+				<th><?php echo __('SR'); ?></th>
+				<th><?php echo __('Remark'); ?></th>
+				<th><?php echo __('Aktionen'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			//print_r($assignments);
+			foreach ($assignments as $assignment): ?>
+				<tr>
+					<td><?php echo h($this->Time->format('D d.m.Y', $assignment['Assignment']['datetime'])); ?></td>
+					<td><?php echo h($this->Time->format('H:i', $assignment['Assignment']['datetime'])); ?></td>
+					<td><?php echo h($assignment['Assignment']['game_number']); ?></td>
+					<td><?php echo h($assignment['League']['League']['title']); ?></td>
+					<td><?php echo h($assignment['Home']['Team']['number']); ?></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td class="actions">
+						<?php echo $this->Html->link(__('View'), array('action' => 'view', $assignment['Assignment']['id'])); ?>
+						<?php echo $this->Html->link(__('iCal File'), array('action' => 'ics', $assignment['Assignment']['id'])); ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+</div>
+
