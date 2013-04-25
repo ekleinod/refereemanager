e5e954e05a1db4fa531e9932e1a622f420f29073
Ekkart Kleinod <ekleinod@edgesoft.de>
2013-04-25 20:37:35 +0200

    defaults for the db in two languages

---
+++ configuration/de/defaults.sql
@@ -0,0 +1,103 @@
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_sex_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (1, 'weiblich', 'Mädchen und Frauen.');
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (2, 'männlich', 'Jungen und Männer.');
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (3, 'andere', 'Menschen anderen Geschlechts.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_contact_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (1, 'privat', 'Privater Kontakt.');
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (2, 'geschäftlich', 'Geschäftskontakt.');
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (3, 'andere', 'Anderer Kontakt.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_league_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (1, 'Frauen', 'Frauenliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (2, 'Herren', 'Herrenliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (3, 'Mädchen', 'Mädchenliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (4, 'Jungen', 'Jungenliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (5, 'Schülerinnen', 'Schülerinnnenliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (6, 'Schüler', 'Schülerliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (7, 'Freizeit', 'Freizeitliga.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (8, 'Senioren', 'Seniorenliga.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_training_level_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (1, 'association umpire', 'aump', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (2, 'national umpire', 'nump', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (3, 'national referee', 'nref', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (4, 'international umpire', 'iu', NULL);
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_status_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (1, 'viele', 'Schiedsrichter_in ist an besonders vielen Einsätzen interessiert.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (2, 'normal', NULL);
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (3, 'nicht aktiv diese Saison', 'Schiedsrichter_in ist diese Saison nicht aktiv.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (4, 'nicht aktiv', 'Schiedsrichter_in ist nicht aktiv.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (5, 'anderes', NULL);
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_referee_assignment_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (1, 'SR', 'Schiedsrichter_in.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (2, 'SRAss', 'Schiedsrichter_in-Assistent_in.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (3, 'OSR', 'Oberschiedsrichter_in.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (4, 'St. OSR', 'Stellvertretende_r Oberschiedsrichter_in.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (5, 'SK', 'Schlägerkontrolle.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (6, 'TM', 'Turniermanagement.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_league_game_team_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_league_game_team_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (1, 'H', 'Heimmannschaft.');
+INSERT INTO `rfrmgr_league_game_team_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (2, 'A', 'Auswärtsmannschaft.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_assignment_status_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (1, 'noch kein Kontakt', 'Schiedsrichter_in wurde noch nicht kontaktiert.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (2, 'nein', 'Schiedsrichter_in nimmt Einsatz nicht wahr.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (3, 'evtl.', 'Schiedsrichter_in hat sich noch nicht entschieden.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (4, 'ja', 'Schiedsrichter_in nimmt Einsatz wahr.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Daten für die Tabelle `rfrmgr_contact_remark_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'AB', 'Auf Anrufbeantworter gesprochen.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'Rückruf', 'Schiedsrichter_in möchte zurückgerufen werden.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'Mail zurück', 'Mail wurde nicht angenommen.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'anderes', NULL);
+
+COMMIT;
+
+
---
+++ configuration/en/defaults.sql
@@ -0,0 +1,103 @@
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_sex_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (1, 'female', 'Female people.');
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (2, 'male', 'Male people.');
+INSERT INTO `rfrmgr_sex_types` (`id`, `title`, `remark`) VALUES (3, 'other', 'People of other sexes.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_contact_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (1, 'private', 'Private contacts.');
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (2, 'business', 'Business contacts.');
+INSERT INTO `rfrmgr_contact_types` (`id`, `title`, `remark`) VALUES (3, 'other', 'Other contacts.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_league_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (1, 'women', 'Women''s leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (2, 'men', 'Men''s leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (3, 'girls', 'Girls'' leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (4, 'boys', 'Boys'' leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (5, 'girl students', 'Girl students'' leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (6, 'boy students', 'Boy students'' leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (7, 'recreational', 'Recreational leagues.');
+INSERT INTO `rfrmgr_league_types` (`id`, `title`, `remark`) VALUES (8, 'seniors', 'Senior''s leagues.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_training_level_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (1, 'association umpire', 'aump', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (2, 'national umpire', 'nump', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (3, 'national referee', 'nref', NULL);
+INSERT INTO `rfrmgr_training_level_types` (`id`, `title`, `abbreviation`, `remark`) VALUES (4, 'international umpire', 'iu', NULL);
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_status_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (1, 'many assignments', 'The referee is interested in many assigments.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (2, 'normal', NULL);
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (3, 'not active this season', 'The referee is not active in this season.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (4, 'not active', 'The referee is not active.');
+INSERT INTO `rfrmgr_status_types` (`id`, `title`, `remark`) VALUES (5, 'other', NULL);
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_referee_assignment_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (1, 'ump', 'Umpire.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (2, 'ass. ump', 'Assistant umpire.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (3, 'ref', 'Referee.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (4, 'sby. ref', 'Standby referee.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (5, 'rc', 'Racket control.');
+INSERT INTO `rfrmgr_referee_assignment_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (6, 'mgr', 'Tournament management.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_league_game_team_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_league_game_team_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (1, 'home', 'Home team.');
+INSERT INTO `rfrmgr_league_game_team_types` (`id`, `abbreviation`, `title`, `remark`) VALUES (2, 'off', 'Off team.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_assignment_status_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (1, 'not contacted yet', 'No contact made to the referee yet.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (2, 'no', 'Referee does not take the assignment.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (3, 'maybe', 'Referee may take the assignment.');
+INSERT INTO `rfrmgr_assignment_status_types` (`id`, `title`, `remark`) VALUES (4, 'yes', 'Referee does take the assignment.');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `rfrmgr_contact_remark_types`
+-- -----------------------------------------------------
+START TRANSACTION;
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'answering machine', 'Spoke to the answering machine.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'call back', 'Referee wants to be called back.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'mail returned', 'Mail was returned.');
+INSERT INTO `rfrmgr_contact_remark_types` (`id`, `title`, `remark`) VALUES (1, 'other', NULL);
+
+COMMIT;
+
+
