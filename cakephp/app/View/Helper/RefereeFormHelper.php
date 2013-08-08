d3837219cd8931a0b314e57b2ea8ea4257ef651c
Ekkart Kleinod <ekleinod@edgesoft.de>
2013-08-08 16:59:41 +0200

    created form input with form helper's input method

--- cakephp/app/View/Helper/RefereeFormHelper.php
+++ cakephp/app/View/Helper/RefereeFormHelper.php
@@ -21,19 +21,18 @@ class RefereeFormHelper extends AppHelper {
 	 */
 	public function getInputText($action, $fieldid, $title, $value = '', $required = false, $placeholder = '', $maxlength = 100, $autofocus = false) {
 
-		// css class
 		$cssclass = 'input text';
 
-		// label
-		$label = $this->Form->label($fieldid, $title);
-
-		// input
 		$inputparams = array();
 		$inputparams['type'] = 'text';
+
 		$inputparams['title'] = $title;
+		$inputparams['label'] = $title;
+
 		if (!empty($value)) {
 			$inputparams['value'] = $value;
 		}
+
 		if (($action !== 'view') && $required) {
 			$inputparams['required'] = 'required';
 			$cssclass .= ' required';
@@ -41,6 +40,7 @@ class RefereeFormHelper extends AppHelper {
 		if (($action !== 'view') && !empty($placeholder)) {
 			$inputparams['placeholder'] = $placeholder;
 		}
+
 		if (!empty($maxlength)) {
 			$inputparams['maxlength'] = $maxlength;
 		}
@@ -52,13 +52,11 @@ class RefereeFormHelper extends AppHelper {
 			$inputparams['readonly'] = 'readonly';
 		}
 
-		$input = $this->Form->text($fieldid, $inputparams);
+		$inputparams['div'] = false;
+		$inputparams['before'] = $this->Html->tag('li', null, array('class' => $cssclass));
+		$inputparams['after'] = '</li>';
 
-		// tag
-		return $this->Html->tag('li',
-				$label . $input,
-				array('class' => $cssclass)
-		);
+		return $this->Form->input($fieldid, $inputparams);
 
 	}
 
