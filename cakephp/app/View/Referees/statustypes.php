1899e8135ad633eaa817bf9cb20a99ab64883eea
Ekkart Kleinod <ekleinod@edgesoft.de>
2015-01-22 21:07:40 +0100

    renamed loop variable

--- cakephp/app/View/Referees/statustypes.php
+++ cakephp/app/View/Referees/statustypes.php
@@ -2,32 +2,32 @@
 
 // Computes different output styles
 
-foreach ($statustypes as &$statustypedef) {
+foreach ($statustypes as &$statustypeedit) {
 
-	$statustypedef['outputstyle'] = '';
+	$statustypeedit['outputstyle'] = '';
 
-	if ($statustypedef['StatusType']['style']) {
-		switch ($statustypedef['StatusType']['style']) {
+	if ($statustypeedit['StatusType']['style']) {
+		switch ($statustypeedit['StatusType']['style']) {
 			case 'normal':
 			case 'italic':
 			case 'oblique':
-				$statustypedef['outputstyle'] .= sprintf('font-style: %s; ', $statustypedef['StatusType']['style']);
+				$statustypeedit['outputstyle'] .= sprintf('font-style: %s; ', $statustypeedit['StatusType']['style']);
 				break;
 			case 'normal':
 			case 'bold':
 			case 'bolder':
 			case 'lighter':
-				$statustypedef['outputstyle'] .= sprintf('font-weight: %s; ', $statustypedef['StatusType']['style']);
+				$statustypeedit['outputstyle'] .= sprintf('font-weight: %s; ', $statustypeedit['StatusType']['style']);
 				break;
 		}
 	}
 
-	if ($statustypedef['StatusType']['color']) {
-		$statustypedef['outputstyle'] .= sprintf('color: #%s; ', $statustypedef['StatusType']['color']);
+	if ($statustypeedit['StatusType']['color']) {
+		$statustypeedit['outputstyle'] .= sprintf('color: #%s; ', $statustypeedit['StatusType']['color']);
 	}
 
-	if ($statustypedef['StatusType']['bgcolor']) {
-		$statustypedef['outputstyle'] .= sprintf('background-color: #%s; ', $statustypedef['StatusType']['bgcolor']);
+	if ($statustypeedit['StatusType']['bgcolor']) {
+		$statustypeedit['outputstyle'] .= sprintf('background-color: #%s; ', $statustypeedit['StatusType']['bgcolor']);
 	}
 
 }
