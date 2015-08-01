dec427390c7322dcc5b0d6557df9cb63722c9fd7
Ekkart Kleinod <ekleinod@edgesoft.de>
2015-08-01 02:08:17 +0200

    new Season from July

--- cakephp/app/Model/Season.php
+++ cakephp/app/Model/Season.php
@@ -80,19 +80,21 @@ class Season extends AppModel {
 	 */
 	public function getSeason($year, $isEditor) {
 
-		$theYear = ($year == null) ? sprintf('%d', ((idate('m') < 8) ? (idate('Y') - 1) : idate('Y'))) : $year;
+		$newSeason = 7;
+
+		$theYear = ($year == null) ? sprintf('%d', ((idate('m') < $newSeason) ? (idate('Y') - 1) : idate('Y'))) : $year;
 
 		$this->recursive = -1;
 		$ssnReturn = $this->findByYearStart($theYear);
 
 		if (($ssnReturn == null) && ($year != null)) {
-			$theYear = sprintf('%d', ((idate('m') < 8) ? (idate('Y') - 1) : idate('Y')));
+			$theYear = sprintf('%d', ((idate('m') < $newSeason) ? (idate('Y') - 1) : idate('Y')));
 			$this->recursive = -1;
 			$ssnReturn = $this->findByYearStart($theYear);
 		}
 
 		if (($ssnReturn != null) && $ssnReturn['Season']['editor_only'] && !$isEditor) {
-			$theYear = sprintf('%d', ((idate('m') < 8) ? (idate('Y') - 1) : idate('Y')));
+			$theYear = sprintf('%d', ((idate('m') < $newSeason) ? (idate('Y') - 1) : idate('Y')));
 			$this->recursive = -1;
 			$ssnReturn = $this->findByYearStart($theYear);
 		}
