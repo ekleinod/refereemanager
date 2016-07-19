55235326bd34895e85eb36f4bbab39533b63a5ae
Ekkart Kleinod <ekkart.kleinod@fokus.fraunhofer.de>
2016-07-19 17:09:34 +0200

    small code bit for getting class properties

--- java-backend/refereemanager/src/test/java/de/edgesoft/refereemanager/model/SeasonModelTest.java
+++ java-backend/refereemanager/src/test/java/de/edgesoft/refereemanager/model/SeasonModelTest.java
@@ -1,47 +1,53 @@
 package de.edgesoft.refereemanager.model;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Arrays;
+import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import de.edgesoft.refereemanager.utils.Constants;
+
 /**
  * Unit test for SeasonModel.
- * 
+ *
  * ## Legal stuff
- * 
+ *
  * Copyright 2016-2016 Ekkart Kleinod <ekleinod@edgesoft.de>
- * 
+ *
  * This file is part of refereemanager.
- * 
+ *
  * refereemanager is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * refereemanager is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with refereemanager.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @author Ekkart Kleinod
  * @version 0.5.0
  * @since 0.5.0
  */
 public class SeasonModelTest {
-	
+
 	/**
 	 * Rule for expected exception
 	 */
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
-	
+
 	/**
 	 * Tests read file not found.
 	 */
@@ -51,21 +57,21 @@ public class SeasonModelTest {
 		exception.expect(NullPointerException.class);
 		exception.expectMessage("date must not be null");
 		SeasonModel.getStartYearForDate(null);
-		
+
 	}
-	
+
 	/**
 	 * Tests getCurrentStartYear.
 	 */
 	@Test
 	public void testGetCurrentStartYear() {
-		
+
 		Integer iTest = SeasonModel.getCurrentStartYear();
-		
+
 		int iExpected = (LocalDate.now().getMonth().ordinal() < SeasonModel.NEWSEASON.ordinal()) ? LocalDate.now().getYear() - 1 : LocalDate.now().getYear();
-		
+
 		Assert.assertEquals(Integer.valueOf(iExpected), iTest);
-		
+
 	}
 
 	/**
@@ -73,15 +79,37 @@ public class SeasonModelTest {
 	 */
 	@Test
 	public void testGetStartYearForDate() {
-		
+
 		for (Month theMonth : Month.values()) {
-			
+
 			Integer iTest = SeasonModel.getStartYearForDate(LocalDate.of(2015, theMonth.getValue(), 01));
-			
+
 			Assert.assertEquals(Integer.valueOf((theMonth.ordinal() < SeasonModel.NEWSEASON.ordinal()) ? 2014 : 2015), iTest);
-			
+
 		}
-		
+
+	}
+
+	/**
+	 * Test.
+	 */
+	@Test
+	public void testUnrelated() {
+
+		try {
+
+			Arrays.asList(
+					Introspector.getBeanInfo(SeasonModel.class).getPropertyDescriptors()
+			)
+					.stream()
+					.filter(pd -> Objects.nonNull(pd.getReadMethod()))
+					.filter(pd -> Objects.nonNull(pd.getWriteMethod()))
+					.forEach(Constants.logger::info);
+
+		} catch (IntrospectionException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
