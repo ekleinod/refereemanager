2038826cd02bd49fa31c87e387a1ecd39f271cf5
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-04-02 02:18:54 +0200

    first statistics, not happy yet

--- refereemanager/src/main/java/de/edgesoft/refereemanager/controller/StatisticsController.java
+++ refereemanager/src/main/java/de/edgesoft/refereemanager/controller/StatisticsController.java
@@ -1,12 +1,33 @@
 package de.edgesoft.refereemanager.controller;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.CategoryChartBuilder;
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.PieChartBuilder;
+import org.knowm.xchart.PieSeries.PieSeriesRenderStyle;
+import org.knowm.xchart.VectorGraphicsEncoder;
+import org.knowm.xchart.VectorGraphicsEncoder.VectorGraphicsFormat;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.internal.chartpart.Chart;
+import org.knowm.xchart.style.PieStyler.AnnotationType;
+
+import de.edgesoft.edgeutils.xchart.BaseTheme;
+import de.edgesoft.edgeutils.xchart.PieTheme;
+import de.edgesoft.refereemanager.model.AppModel;
+import de.edgesoft.refereemanager.model.ContentModel;
+import de.edgesoft.refereemanager.model.RefereeModel;
 import de.edgesoft.refereemanager.utils.Resources;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -14,7 +35,7 @@ import javafx.stage.Stage;
  *
  * ## Legal stuff
  *
- * Copyright 2016-2016 Ekkart Kleinod <ekleinod@edgesoft.de>
+ * Copyright 2016-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
  *
  * This file is part of TT-Schiri: Referee Manager.
  *
@@ -32,37 +53,46 @@ import javafx.stage.Stage;
  * along with TT-Schiri: Referee Manager. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Ekkart Kleinod
- * @version 0.10.0
+ * @version 0.14.0
  * @since 0.10.0
  */
 public class StatisticsController {
 
 	/**
-	 * Bar chart overview.
+	 * Tab pane.
 	 *
-	 * @version 0.10.0
-	 * @since 0.10.0
+	 * @version 0.14.0
+	 * @since 0.14.0
 	 */
 	@FXML
-	private BarChart<String, Integer> chartOverview;
+	private TabPane pneTabs;
 
 	/**
-	 * Pie chart gender.
+	 * Chart overview.
 	 *
-	 * @version 0.10.0
+	 * @version 0.14.0
 	 * @since 0.10.0
 	 */
 	@FXML
-	private PieChart chartGender;
+	private SwingNode nodeOverview;
 
 	/**
-	 * Pie chart training levels.
+	 * Chart gender.
 	 *
-	 * @version 0.10.0
+	 * @version 0.14.0
 	 * @since 0.10.0
 	 */
 	@FXML
-	private PieChart chartTrainingLevel;
+	private SwingNode nodeGender;
+
+	/**
+	 * Chart training levels.
+	 *
+	 * @version 0.14.0
+	 * @since 0.10.0
+	 */
+	@FXML
+	private SwingNode nodeTrainingLevel;
 
 	/**
 	 * Tab overview.
@@ -101,6 +131,16 @@ public class StatisticsController {
 	private Button btnOK;
 
 	/**
+	 * Export button.
+	 *
+	 * @version 0.14.0
+	 * @since 0.14.0
+	 */
+	@FXML
+	private Button btnExport;
+
+
+	/**
 	 * Reference to dialog stage.
 	 *
 	 * @version 0.10.0
@@ -118,8 +158,8 @@ public class StatisticsController {
 	 * @since 0.10.0
 	 */
 	public void setDialogStage(final Stage theStage) {
-				dialogStage = theStage;
-		}
+		dialogStage = theStage;
+	}
 
 	/**
 	 * Initializes the controller class.
@@ -144,57 +184,76 @@ public class StatisticsController {
 	/**
 	 * Fills statistics.
 	 *
-	 * @version 0.10.0
+	 * @version 0.14.0
 	 * @since 0.10.0
 	 */
 	public void fillStatistics() {
 
-//		// compute statistics data
-//		Map<String, Map<Month, AtomicInteger>> mapCounts = new HashMap<>();
-//
-//		theData.forEach(event -> {
-//			mapCounts.computeIfAbsent(event.getEventtype().getValue(), eventtype -> {
-//				Map<Month, AtomicInteger> mapMonthCounts = new HashMap<>();
-//				for (Month month : Month.values()) {
-//					mapMonthCounts.put(month, new AtomicInteger());
-//				}
-//				return mapMonthCounts;
-//			});
-//			mapCounts.get(event.getEventtype().getValue()).get(((LocalDate) event.getDate().getValue()).getMonth()).incrementAndGet();
-//		});
-//
-//		AtomicInteger iWholeCount = new AtomicInteger();
-//
-//		// output data
-//		mapCounts.entrySet().stream()
-//				.sorted(Comparator.comparing(typemap -> typemap.getKey(), Collator.getInstance()))
-//				.forEach(typemap -> {
-//
-//					AtomicInteger iEventCount = new AtomicInteger();
-//
-//					XYChart.Series<String, Integer> seriesOverview = new XYChart.Series<>();
-//					seriesOverview.setName(typemap.getKey());
-//
-//					XYChart.Series<String, Integer> seriesStacked = new XYChart.Series<>();
-//					seriesStacked.setName(typemap.getKey());
-//
-//					typemap.getValue().entrySet().stream()
-//							.sorted(Comparator.comparing(monthcount -> monthcount.getKey()))
-//									.forEach(monthcount -> {
-//										seriesOverview.getData().add(new XYChart.Data<>(monthcount.getKey().getDisplayName(TextStyle.FULL, Locale.getDefault()), monthcount.getValue().get()));
-//										seriesStacked.getData().add(new XYChart.Data<>(monthcount.getKey().getDisplayName(TextStyle.FULL, Locale.getDefault()), monthcount.getValue().get()));
-//										iEventCount.addAndGet(monthcount.getValue().get());
-//									});
-//
-//					chartOverview.getData().add(seriesOverview);
-//					chartGender.getData().add(seriesStacked);
-//					chartTrainingLevel.getData().add(new PieChart.Data(String.format("%s (%d)", typemap.getKey(), iEventCount.get()), iEventCount.get()));
-//
-//					iWholeCount.addAndGet(iEventCount.get());
-//
-//				});
+		ContentModel theContent = (ContentModel) AppModel.getData().getContent();
 
-		}
+		// overview
+	    CategoryChart chartOverview = new CategoryChartBuilder()
+	    		.title(tabOverview.getText())
+	    		.build();
+
+	    chartOverview.getStyler().setTheme(new BaseTheme());
+
+	    chartOverview.addSeries("test",
+	    		Arrays.asList(new String[] {"Schiedsrichter", "Auszubildende", "Personen", "Clubs", "Teams", "Ligen"}),
+	    		Arrays.asList(new Number[] {theContent.getReferee().size(), theContent.getTrainee().size(), theContent.getPerson().size(), theContent.getClub().size(), theContent.getTeam().size(), theContent.getLeague().size()})
+				);
+
+	    nodeOverview.setContent(new XChartPanel<>(chartOverview));
+
+		// gender
+	    PieChart chartGender = new PieChartBuilder()
+	    		.title(tabGender.getText())
+	    		.build();
+
+	    chartGender.getStyler().setTheme(new PieTheme());
+	    chartGender.getStyler().setAnnotationType(AnnotationType.Label);
+	    chartGender.getStyler().setDefaultSeriesRenderStyle(PieSeriesRenderStyle.Donut);
+
+	    theContent.getSexType().forEach(sextype -> {
+
+	    	int iSize = theContent.getReferee().stream()
+	    			.filter(referee -> referee.getSexType().equals(sextype))
+	    			.collect(Collectors.toList())
+	    			.size();
+
+	    	if (iSize > 0) {
+	    		chartGender.addSeries(sextype.getDisplayText().getValue(), iSize);
+	    	}
+
+	    });
+
+	    nodeGender.setContent(new XChartPanel<>(chartGender));
+
+		// level
+	    PieChart chartTrainingLevel = new PieChartBuilder()
+	    		.title(tabTrainingLevel.getText())
+	    		.build();
+
+	    chartTrainingLevel.getStyler().setTheme(new PieTheme());
+	    chartTrainingLevel.getStyler().setAnnotationType(AnnotationType.Label);
+	    chartTrainingLevel.getStyler().setDefaultSeriesRenderStyle(PieSeriesRenderStyle.Donut);
+
+	    theContent.getTrainingLevelType().forEach(trainingleveltype -> {
+
+	    	int iSize = theContent.getReferee().stream()
+	    			.filter(referee -> ((RefereeModel) referee).getHighestTrainingLevel().getType().equals(trainingleveltype))
+	    			.collect(Collectors.toList())
+	    			.size();
+
+	    	if (iSize > 0) {
+	    		chartTrainingLevel.addSeries(trainingleveltype.getDisplayText().getValue(), iSize);
+	    	}
+
+	    });
+
+	    nodeTrainingLevel.setContent(new XChartPanel<>(chartTrainingLevel));
+
+	}
 
 	/**
 	 * Closes dialog.
@@ -203,9 +262,28 @@ public class StatisticsController {
 	 * @since 0.10.0
 	 */
 	@FXML
-		private void handleOk() {
-				dialogStage.close();
+	private void handleOk() {
+		dialogStage.close();
+	}
+
+	/**
+	 * Exports chart.
+	 *
+	 * @version 0.14.0
+	 * @since 0.14.0
+	 */
+	@FXML
+	private void handleExport() {
+
+		Chart<?, ?> activeChart = ((XChartPanel<?>) ((SwingNode) ((AnchorPane) pneTabs.getSelectionModel().getSelectedItem().getContent()).getChildren().get(0)).getContent()).getChart();
+
+		try {
+			VectorGraphicsEncoder.saveVectorGraphic(activeChart, "/home/ekleinod/working/rani/schiri/refereemanager/generiert/test", VectorGraphicsFormat.PDF);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+	}
 
 }
 
