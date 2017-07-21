13a079bfd6b9fa2a520b7e331f50b7a301212f39
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-07-21 18:51:02 +0200

    more attributes for referee assignments

--- refereemanager/src/main/java/de/edgesoft/refereemanager/jaxb/RefereeAssignment.java
+++ refereemanager/src/main/java/de/edgesoft/refereemanager/jaxb/RefereeAssignment.java
@@ -1,13 +1,18 @@
 
 package de.edgesoft.refereemanager.jaxb;
 
+import java.time.LocalDateTime;
+import javafx.beans.property.SimpleBooleanProperty;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import de.edgesoft.edgeutils.commons.AbstractModelClass;
+import de.edgesoft.edgeutils.commons.ext.LocalDateTimeAdapter;
+import de.edgesoft.edgeutils.javafx.SimpleBooleanPropertyAdapter;
 
 
 /**
@@ -20,6 +25,8 @@ import de.edgesoft.edgeutils.commons.AbstractModelClass;
  *   &lt;complexContent>
  *     &lt;extension base="{}AbstractModelClass">
  *       &lt;sequence>
+ *         &lt;element name="modified" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="active" type="{}BooleanProperty"/>
  *         &lt;element name="referee" type="{http://www.w3.org/2001/XMLSchema}IDREF"/>
  *         &lt;element name="referee_assigment_type" type="{http://www.w3.org/2001/XMLSchema}IDREF"/>
  *       &lt;/sequence>
@@ -32,6 +39,8 @@ import de.edgesoft.edgeutils.commons.AbstractModelClass;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "RefereeAssignment", propOrder = {
+    "modified",
+    "active",
     "referee",
     "refereeAssigmentType"
 })
@@ -39,6 +48,14 @@ public class RefereeAssignment
     extends AbstractModelClass
 {
 
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(LocalDateTimeAdapter.class)
+    @XmlSchemaType(name = "dateTime")
+    protected LocalDateTime modified;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(SimpleBooleanPropertyAdapter.class)
+    @XmlSchemaType(name = "boolean")
+    protected SimpleBooleanProperty active;
     @XmlElement(required = true, type = Object.class)
     @XmlIDREF
     @XmlSchemaType(name = "IDREF")
@@ -47,6 +64,54 @@ public class RefereeAssignment
     @XmlIDREF
     @XmlSchemaType(name = "IDREF")
     protected RefereeAssignmentType refereeAssigmentType;
+
+    /**
+     * Gets the value of the modified property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    /**
+     * Sets the value of the modified property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setModified(LocalDateTime value) {
+        this.modified = value;
+    }
+
+    /**
+     * Gets the value of the active property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public SimpleBooleanProperty getActive() {
+        return active;
+    }
+
+    /**
+     * Sets the value of the active property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setActive(SimpleBooleanProperty value) {
+        this.active = value;
+    }
 
     /**
      * Gets the value of the referee property.
