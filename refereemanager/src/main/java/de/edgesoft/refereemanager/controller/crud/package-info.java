8b95df90569642f4c84dedac1216ac0a7824e224
Ekkart Kleinod <ekleinod@edgesoft.de>
2018-02-20 14:58:37 +0100

    API doc

---
+++ refereemanager/src/main/java/de/edgesoft/refereemanager/controller/crud/package-info.java
@@ -0,0 +1,34 @@
+/**
+ * All CRUD controller classes.
+ *
+ * The application tries to follow the model-view-controller approach.
+ *
+ * This package contains the classes resp. interfaces for CRUD interactions.
+ *
+ * ## Legal stuff
+ *
+ * Copyright 2016-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
+ *
+ * This file is part of TT-Schiri: Referee Manager.
+ *
+ * TT-Schiri: Referee Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TT-Schiri: Referee Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TT-Schiri: Referee Manager. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Ekkart Kleinod
+ * @version 0.15.0
+ * @since 0.15.0
+ */
+
+package de.edgesoft.refereemanager.controller.crud;
+
+/* EOF */
--- refereemanager/src/main/java/de/edgesoft/refereemanager/model/AppModel.java
+++ refereemanager/src/main/java/de/edgesoft/refereemanager/model/AppModel.java
@@ -14,20 +14,20 @@ import de.edgesoft.refereemanager.utils.Prefs;
  *
  * Copyright 2016-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
  *
- * This file is part of "Das Gebu-Programm".
+ * This file is part of TT-Schiri: Referee Manager.
  *
- * "Das Gebu-Programm" is free software: you can redistribute it and/or modify
+ * TT-Schiri: Referee Manager is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * "Das Gebu-Programm" is distributed in the hope that it will be useful,
+ * TT-Schiri: Referee Manager is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with "Das Gebu-Programm".  If not, see <http://www.gnu.org/licenses/>.
+ * along with TT-Schiri: Referee Manager. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Ekkart Kleinod
  * @version 0.14.0
