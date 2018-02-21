deecc9fecc3ef5f431837c29a36e53472d3bbcbd
Ekkart Kleinod <ekleinod@edgesoft.de>
2018-02-22 00:24:42 +0100

    first steps for unit testing

---
+++ refereemanager/src/test/java/de/edgesoft/refereemanager/RefereeManagerTest.java
@@ -0,0 +1,74 @@
+package de.edgesoft.refereemanager;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * RefereeManager application test.
+ *
+ * ## Legal stuff
+ *
+ * Copyright 2016-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
+ *
+ * This file is part of TT-Schiri: Referee Manager.
+ *
+ * TT-Schiri: Referee Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TT-Schiri: Referee Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TT-Schiri: Referee Manager. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Ekkart Kleinod
+ * @version 0.15.0
+ * @since 0.15.0
+ */
+@ExtendWith(ApplicationExtension.class)
+public class RefereeManagerTest {
+
+	Button button;
+
+	@Start
+    void onStart(Stage stage) {
+        button = new Button("click me!");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+        stage.setScene(new Scene(new StackPane(button), 100, 100));
+        stage.show();
+    }
+
+    @Test
+    void should_contain_button() {
+        // expect:
+    	verifyThat(".button", hasText("click me!"));
+    }
+
+    @Test
+    void should_click_on_button(FxRobot robot) {
+        // when:
+        robot.clickOn(".button");
+
+        // then:
+        verifyThat(".button", hasText("clicked!"));
+    }
+
+}
+
+/* EOF */
