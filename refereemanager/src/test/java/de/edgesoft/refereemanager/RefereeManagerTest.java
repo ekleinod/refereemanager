02150bf952cf342b856df7d87a44efdd8a741846
Ekkart Kleinod <ekleinod@edgesoft.de>
2018-02-26 00:39:30 +0100

    typing in standard dialog works now

--- refereemanager/src/test/java/de/edgesoft/refereemanager/RefereeManagerTest.java
+++ refereemanager/src/test/java/de/edgesoft/refereemanager/RefereeManagerTest.java
@@ -4,11 +4,13 @@ package de.edgesoft.refereemanager;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.testfx.api.FxAssert.verifyThat;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeoutException;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.InvalidPreferencesFormatException;
@@ -25,8 +27,9 @@ import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.matcher.control.ButtonMatchers;
 import org.testfx.matcher.control.TextInputControlMatchers;
 
+import de.edgesoft.edgeutils.testfx.CheckBoxMatcher;
+import de.edgesoft.edgeutils.testfx.RobotHelper;
 import javafx.application.Application;
-import javafx.scene.control.CheckBox;
 import javafx.scene.input.KeyCode;
 
 /**
@@ -59,19 +62,29 @@ import javafx.scene.input.KeyCode;
 public class RefereeManagerTest {
 
 	/**
+	 * Easing debugging of test with flags: no export of preferences at start of test.
+	 */
+	private static final boolean DEBUG_EXPORT_PREFS = false;
+
+	/**
+	 * Easing debugging of test with flags: no import of preferences at end of test.
+	 */
+	private static final boolean DEBUG_IMPORT_PREFS = false;
+
+	/**
 	 * Sleep time in ms.
 	 */
-	public static final int SLEEP = 1000;
+	private static final int SLEEP = 1000;
 
 	/**
 	 * File name for temporary preferences file.
 	 */
-	public static final String FILE_PREFS = "savedPreferences.preferences";
+	private static final String FILE_PREFS = "savedPreferences.preferences";
 
 	/**
 	 * File name for temporary content file.
 	 */
-	public static final String FILE_CONTENT = "RefManContent.xml";
+	private static final String FILE_CONTENT = "RefManContent.xml";
 
 	/**
 	 * Application.
@@ -82,6 +95,7 @@ public class RefereeManagerTest {
 	 * Init tests - call application.
 	 *
 	 * - save current preferences
+	 * - if exists - remove test file
 	 * - start app
 	 *
 	 * @throws TimeoutException
@@ -89,14 +103,23 @@ public class RefereeManagerTest {
 	@BeforeAll
     public static void initAll() throws TimeoutException {
 
-		try (OutputStream osPrefs = new FileOutputStream(FILE_PREFS)) {
-			Preferences.userNodeForPackage(RefereeManager.class).exportNode(osPrefs);
-		} catch (IOException | BackingStoreException e) {
-			e.printStackTrace();
+		if (DEBUG_EXPORT_PREFS) {
+			try (OutputStream osPrefs = new FileOutputStream(FILE_PREFS)) {
+				Preferences.userNodeForPackage(RefereeManager.class).exportNode(osPrefs);
+			} catch (IOException | BackingStoreException e) {
+				e.printStackTrace();
+			}
+		}
+
+		File fleTemp = Paths.get(FILE_CONTENT).toFile();
+		if (fleTemp.exists()) {
+			fleTemp.delete();
 		}
 
         FxToolkit.registerPrimaryStage();
         appRefMan = FxToolkit.setupApplication(RefereeManager.class);
+
+
     }
 
 	/**
@@ -143,11 +166,11 @@ public class RefereeManagerTest {
 		}
 
     	// preferences
-    	robot.clickOn("#mnuProgram").clickOn("#mnuProgramPreferences");
+    	robot.clickOn("#btnProgramPreferences");
     	robot.sleep(SLEEP);
 
-    	verifyThat("#chkTitleFullpath", (CheckBox box) -> !box.isSelected());
-    	verifyThat("#chkDataSortLoading", (CheckBox box) -> !box.isSelected());
+    	verifyThat("#chkTitleFullpath", CheckBoxMatcher.isNotSelected());
+    	verifyThat("#chkDataSortLoading", CheckBoxMatcher.isNotSelected());
     	verifyThat("#txtPathsTemplate", TextInputControlMatchers.hasText(""));
     	verifyThat("#txtRefereeReportPath", TextInputControlMatchers.hasText(""));
     	verifyThat("#txtRefereeReportTournament", TextInputControlMatchers.hasText("OSR_%1$s_%2$s.pdf"));
@@ -161,12 +184,12 @@ public class RefereeManagerTest {
     	robot.sleep(SLEEP);
 
 		// save empty file
-//    	robot.clickOn("#btnFileSave");
-//    	robot.sleep(SLEEP);
-//    	robot.write(FILE_CONTENT);
-//    	robot.sleep(SLEEP*5);
-//    	robot.push(KeyCode.ENTER);
-//    	robot.sleep(SLEEP*5);
+    	robot.clickOn("#btnFileSave");
+    	robot.sleep(SLEEP);
+    	RobotHelper.write(robot, FILE_CONTENT);
+    	robot.sleep(SLEEP);
+    	robot.push(KeyCode.ENTER);
+    	robot.sleep(SLEEP);
 
     }
 
@@ -181,11 +204,13 @@ public class RefereeManagerTest {
 	@AfterAll
 	public static void tearDownAll() throws TimeoutException {
 
-		try (InputStream isPrefs = new FileInputStream(FILE_PREFS)) {
-			Preferences.userNodeForPackage(RefereeManager.class).clear();
-			Preferences.importPreferences(isPrefs);
-		} catch (IOException | BackingStoreException | InvalidPreferencesFormatException e) {
-			e.printStackTrace();
+		if (DEBUG_IMPORT_PREFS) {
+			try (InputStream isPrefs = new FileInputStream(FILE_PREFS)) {
+				Preferences.userNodeForPackage(RefereeManager.class).clear();
+				Preferences.importPreferences(isPrefs);
+			} catch (IOException | BackingStoreException | InvalidPreferencesFormatException e) {
+				e.printStackTrace();
+			}
 		}
 
 		FxToolkit.cleanupApplication(appRefMan);
